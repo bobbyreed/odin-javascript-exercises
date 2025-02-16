aeac832281224e75d05a59020a3391afd961a55d
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-02-15 21:21:14 -0600

    completed 03_reverseString exercise for JS Odin Exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let charArray = [];
+    for(let i = 0; i < string.length; i++){
+        charArray[i] = string.charAt(i);
+    }
+    charArray.reverse();
+    return charArray.join("");
 };
 
 // Do not edit below this line
