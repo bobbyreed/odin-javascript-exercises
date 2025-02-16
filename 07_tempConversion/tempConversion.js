70fca76ab50bd4c454b57c3313ad22ffa5676336
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-02-15 23:11:41 -0600

    completed 07_tempConversion exercise for JS Odin Exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Ftemp) {
+  let convertedTemp = (Ftemp - 32)*5/9;
+  if(/\d+\.\d+/.test(convertedTemp)){
+    return parseFloat(convertedTemp.toFixed(1));
+  }
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Ctemp) {
+  let convertedTemp = ((Ctemp * 9/5 + 32));
+  if(/\d+\.\d+/.test(convertedTemp)){
+    return parseFloat(convertedTemp.toFixed(1));
+  }
+  return convertedTemp;
 };
 
 // Do not edit below this line
