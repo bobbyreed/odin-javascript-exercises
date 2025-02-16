c5b65ea249d63eb5c981d7c37d4b149e06712e1f
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-02-15 22:01:32 -0600

    completed 04_removeFromArray exercise for JS Odin Exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for(let j = 0; j < args.length; j++){
+        let numToRemove = args[j];
+        for(let i = 0; i < array.length; i++){
+            if(array[i] == numToRemove && typeof args[j] == typeof array[i]){
+                delete array[i];
+                array.sort();
+                array.pop();
+                i = 0;
+            }
+        }
+        
+    }    
+    return array;
 };
 
 // Do not edit below this line
