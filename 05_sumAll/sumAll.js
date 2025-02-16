be689d2c69fed97de567b1401cd0237bb7b9fc25
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-02-15 22:50:29 -0600

    completed 05_sumAll exercise for JS Odin Exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    let runningSum = 0;
+    if(/\d+\.\d+/.test(args[0]) || args[0] < 0 || /\d+\.\d+/.test(args[1]) || args[1] < 0 || typeof args[0] !== 'number' || typeof args[1] !== 'number'){
+        return "ERROR";
+    }
+    if(args[0]<args[1]){
+        while((args[0])<(args[1]+1)){    
+            runningSum += args[0];
+            args[0]++;
+            }
+    }
+    else if(args[0]>args[1]){
+        while((args[1])<(args[0]+1)){    
+            runningSum += args[1];
+            args[1]++;
+            }
+    }
+    
+        return runningSum;
 };
 
 // Do not edit below this line
