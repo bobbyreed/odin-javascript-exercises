e8aef415461114ba99f2475c0e101b4b397e4977
bobby reed <37113385+bobbyreed@users.noreply.github.com>
2025-04-29 12:48:52 -0500

    completed exercise 12. See note on co-pilot
    Co-pilot note: The co-pilot help was disastrous in this exercise. So interesting given that it was perfect in exercise 11. In this exercise, the co-pilot help created a structure that would ensure that no numbers could calculate NaN.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    oldest = 0;
+    age = 0;
+        for(let i = 0; i < people.length; i++) {
+            console.log(`Checking person: ${people[i].name}`);
+                if (!people[i].yearOfDeath) {
+                    people[i].yearOfDeath = currentYear;
+                    console.log(`Person ${people[i].name} has no yearOfDeath, assuming current year: ${currentYear}`);
+                }
+                // Calculate the age of the person
+                const age = people[i].yearOfDeath - people[i].yearOfBirth;
+                if(age > oldest){
+                    oldest = age;
+                    oldestPerson = people[i];
+                }
+                console.log(`${people[i].name} is ${age}. Current oldest age: ${oldest}`);
+                
+                
+        }
+        return oldestPerson;
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;
