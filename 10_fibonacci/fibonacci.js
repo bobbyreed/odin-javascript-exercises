df6b81a1d7ffcc8be69ad76791db00688579706b
bobby reed <37113385+bobbyreed@users.noreply.github.com>
2025-04-29 12:03:01 -0500

    Completed the 10fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0){
+        return "OOPS";
+    }
+    if(index == 0){
+        return 0;
+    }    
+    fib = [1, 1];
+        if(index <= 2){
+            return 1;
+        }
+        else if(index > 2){
+            for(let i = 2; i < index; i++) {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+            return fib[index - 1];
+        }
 };
 
 // Do not edit below this line
